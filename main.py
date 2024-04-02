9dbc9477e85a797a355391de67996d59916b97a0
kiran14585 <52446694+kiran14585@users.noreply.github.com>
2024-04-02 11:36:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
 
 
 app =  FastAPI()
 
 @app.get("/")
 def test():
-    
     return "<h1 style='color:blue'>Happy birthday Shabeeb!</h1>"
+
+    
 
     
